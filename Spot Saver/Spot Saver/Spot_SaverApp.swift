c75e2125a3988d8ff9967e35afab8c8053396a74
Rishu Bajpai <rishu.bajpai123@gmail.com>
2025-09-24 07:33:35 -0700

    first commit for fourth time

---
+++ Spot Saver/Spot Saver/Spot_SaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spot_SaverApp.swift
+//  Spot Saver
+//
+//  Created by Rishu Bajpai on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Spot_SaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
